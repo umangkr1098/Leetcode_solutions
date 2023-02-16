f5b39012bb95d186f4361a37fca3aaf0ea7df92b
umangkr1098 <95139666+umangkr1098@users.noreply.github.com>
2023-02-16 22:27:27 +0530

    Time: 3 ms, Memory: 8.8 MB - LeetHub

---
+++ remove-element/remove-element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        vector<int> ans;
+        int j=0;
+     for(int i=0;i<nums.size();i++){
+         if(nums[i]==val){
+             j++;  
+         }
+         else{
+            //ans.push_back(nums[i]);
+             nums[i-j]=nums[i];
+         }
+     } 
+        return nums.size()-j;
+    }
+    
+};
