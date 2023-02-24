3da32499f9a2aab42b36d1a6ea1798e077440128
umangkr1098 <95139666+umangkr1098@users.noreply.github.com>
2023-02-24 21:36:15 +0530

    Time: 31 ms, Memory: 19.3 MB - LeetHub

--- move-zeroes/move-zeroes.cpp
+++ move-zeroes/move-zeroes.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
-    void moveZeroes(vector<int>& nums) {
+    void moveZeroes(vector<int>& nums) 
+    {
         int j=0;
         for(int i=0;i<nums.size();i++)
         {   
-            if(nums[i]!=0){
+            if(nums[i]!=0)
+            {
                swap(nums[j],nums[i]);
                 j++;
             }
