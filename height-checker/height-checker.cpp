9d59fa10d3db1beb6738a9df1315840bdca7e355
umangkr1098 <95139666+umangkr1098@users.noreply.github.com>
2023-02-27 22:08:35 +0530

    Time: 3 ms, Memory: 8.3 MB - LeetHub

---
+++ height-checker/height-checker.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> ans;
+        int count =0;
+        ans=heights;
+        
+        sort(heights.begin(),heights.end());
+        
+         for(int i=0;i<heights.size();i++)
+        {
+            if(ans[i]!=heights[i])
+            {
+                count++;
+            }
+        }
+        
+     return count;   
+    }
+    
+};
