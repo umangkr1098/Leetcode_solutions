a384ec377d8beb626e23051c8db2522cfe9579f5
umangkr1098 <95139666+umangkr1098@users.noreply.github.com>
2023-03-05 16:25:21 +0530

    Time: 3 ms (89.34%), Space: 9.6 MB (96.73%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -4,21 +4,16 @@ public:
         int s=0;
         int e=nums.size()-1;
         int mid = s+(e-s)/2;
-        
         while(s<=e)
         {
             if(target==nums[mid])
                 return mid;
-            
             else if(target<nums[mid])
                 e=mid-1;
-            
             else
                 s=mid+1;
-            
             mid = s+(e-s)/2;
-        }
-        
+        }  
     return mid;    
     }
 };
