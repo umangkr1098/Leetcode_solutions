ac6bd94d60c3e2f774be63bb0b4fbab49b14e834
umangkr1098 <95139666+umangkr1098@users.noreply.github.com>
2023-03-05 11:26:44 +0530

    Time: 104 ms (96.61%), Space: 59.7 MB (26.05%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -4,7 +4,6 @@ public:
         int s=0;
         int e = arr.size()-1;
         int mid = s +(e-s)/2; 
-        
         while(s<e)
         {
             if(arr[mid] < arr[mid+1]){
@@ -15,7 +14,6 @@ public:
             }
             mid = s+(e-s)/2;
         }
-        
         return s;
     }
 };
