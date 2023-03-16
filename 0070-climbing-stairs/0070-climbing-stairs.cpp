1aa7c5c82ad68e7f292065c57e94f7efb4fbe30e
umangkr1098 <95139666+umangkr1098@users.noreply.github.com>
2023-03-16 19:58:20 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (95.91%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        
+        if(n==0)
+            return 0;
+        if(n==1)
+            return 1;
+        if(n==2)
+            return 2;
+        int n1=1;
+        int n2=2;
+        int n3;
+        for(int i=2;i<n;i++)
+        {
+           n3=n2+n1;
+           n1=n2;
+            n2=n3;
+        }
+        return n3;
+    }
+};
