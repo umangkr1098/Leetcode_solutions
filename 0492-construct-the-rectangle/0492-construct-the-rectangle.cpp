36d8767d617b640e730c4d5f8d1417808cd50592
umangkr1098 <95139666+umangkr1098@users.noreply.github.com>
2023-03-16 20:38:19 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (93.61%) - LeetHub

--- 0492-construct-the-rectangle/0492-construct-the-rectangle.cpp
+++ 0492-construct-the-rectangle/0492-construct-the-rectangle.cpp
@@ -3,13 +3,11 @@ public:
     vector<int> constructRectangle(int area) {
         vector<int> ans;
         int width=1;
-         for(int i=1;i<=sqrt(area);i++)
-         {
-             if(area%i==0)
-                 width=i;   
+        for(int i=1;i<=sqrt(area);i++)
+         {if(area%i==0)
+            width=i;   
          }
         ans.push_back(area/width);
         ans.push_back(width);
-        return ans;
-    }
+        return ans;}
 };
