dc1fc34675590ea7d4ce8f893d742f5bed77c52a
umangkr1098 <95139666+umangkr1098@users.noreply.github.com>
2023-03-24 23:21:44 +0530

    Time: 0 ms (100.00%), Space: 9.5 MB (59.35%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -6,7 +6,8 @@ public:
         int s=0;
         int e=rows*cols-1;
         while(s<=e)
-        {  int mid = s+(e-s)/2;
+        {  
+            int mid = s+(e-s)/2;
            int rowInd = mid/cols;
            int colInd = mid%cols;
           if(matrix[rowInd][colInd] == target)
@@ -17,5 +18,5 @@ public:
              s=mid+1;    
         }
         return false;
-        }
+    }
 };
